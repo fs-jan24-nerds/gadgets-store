8a65d7f69b9e98ff2a1abf887ef5b5e5e63b4591
Nikita Ganzha <geekneek28@gmail.com>
2024-04-16 14:10:56 +0300

    add <p>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Gadgets Store</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Gadgets Store</h1>
+      <p>Get started by editing src/App.tsx</p>
+    </>
+  );
 }
 
 export default App;
