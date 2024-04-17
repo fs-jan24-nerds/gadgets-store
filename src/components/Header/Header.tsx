25ca9dcb1fd9ae2b76b9e3126b803f5ed292ac80
Oleksandr Yepik <sepika534@gmail.com>
2024-04-17 23:23:40 +0300

    fix bug set the paths to the pictures and fix stule for icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,40 @@
 import { Link, NavLink } from 'react-router-dom';
 import { Navigation } from './Navigation';
+import favourites from '../../assets/icons/favourites.svg';
+import cart from '../../assets/icons/cart.svg';
+import logo from '../../assets/icons/Logo.svg';
+import burgerMenu from '../../assets/icons/Menu.svg';
+
+const getClassIcon = () => {
+  return'border-l-2 border-gray w-16 h-16 flex items-center justify-center';
+};
 
 export const Header = () => {
   return (
     <header className="grid grid-cols-12 w-full items-center border-b-2 border-gray justify-between:sm">
-      <Link to="/" className="block  w-32 h-16 flex items-center justify-center">
-        <img src="/img/icons/Logo.svg" alt="logo" />
+      <Link to="/" className="w-32 h-16 flex items-center justify-center">
+        <img src={logo} alt="logo" />
       </Link>
-      <div className="hidden sm:grid col-start-3 col-end-13 flex justify-between hidden:sm ">
+      <div className="hidden sm:grid col-start-3 col-end-13 justify-between hidden:sm ">
         <Navigation />
         <div className="flex col-end-12 items-center">
           <NavLink
             to="/favorites"
-            className="block border-2 border-gray w-16 h-16 flex items-center justify-center"
+            className={getClassIcon}
           >
-            <img src="/img/icons/favourites.svg" alt="favourites" className="w-6 h-6" />
+            <img src={favourites} alt="favourites" className="w-6 h-6" />
           </NavLink>
           <NavLink
             to="/cart"
-            className="block border-2 border-gray border-l-none w-16 h-16 flex items-center justify-center"
+            className={getClassIcon}
           >
-            <img src="/img/icons/cart.svg" alt="cart" className="w-6 h-6" />
+            <img src={cart} alt="cart" className="w-6 h-6" />
           </NavLink>
         </div>
       </div>
-      <div className="sm:hidden col-end-12   ml-auto">
+      <div className="sm:hidden col-end-12 ml-auto">
         <NavLink to="/">
-          <img src="/img/icons/Menu.svg" alt="menu" className="w-6 h-6" />
+          <img src={burgerMenu} alt="menu" className="w-6 h-6" />
         </NavLink>
       </div>
     </header>
--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -7,12 +7,12 @@ export const Navigation: React.FC = () => {
   const location = useLocation();
   const getClass = ({ isActive }: { isActive: boolean }) =>
     classNames(
-      'navlink hover:border-b-4 border-primary font-bold tracking-wide leading-3 flex flex-col sm:flex-row',
+      'navlink hover:border-b-4 border-primary font-bold tracking-wide leading-3 flex flex-col  sm:flex-row ',
       { 'navlink-active': isActive, 'text-secondary relative': !isActive },
     );
   return (
     <nav>
-      <div className="flex justify space-x-4" >
+      <div className="flex justify   sm:space-x-8 md:space-x-16">
         {Object.values(NavMenu).map((menu) => (
           <NavLink key={menu} to={{ pathname: menu, search: location.search }} className={getClass}>
             {menu.toUpperCase()}
