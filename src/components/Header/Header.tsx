a29a757d4ec7f926c20e49b09f2e18271c164b73
Oleksandr Yepik <sepika534@gmail.com>
2024-04-17 17:06:34 +0300

    add logo and icons for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,28 @@
+import { Link, NavLink } from 'react-router-dom';
 import { Navigation } from './Navigation';
 
 export const Header = () => {
   return (
-    <header className="grid grid-cols-12">
-      <Navigation />
+    <header className="grid grid-cols-12 items-center">
+      <Link to="/">
+        <img src="/img/icons/Logo.svg" alt="logo" className="" />
+      </Link>
+      <div className="col-span-8 flex justify-end hidden sm:flex ">
+        <Navigation />
+        <div className="flex ">
+          <NavLink to="/favorites" className="w-6 h-6">
+            <img src="/img/icons/favourites.png" alt="favourites" />
+          </NavLink>
+          <NavLink to="/cart" className="w-6 h-6">
+            <img src="/img/icons/cart.png" alt="cart" />
+          </NavLink>
+        </div>
+      </div>
+      <div className="flex sm:hidden">
+        <NavLink to="/">
+          <img src="/img/icons/menu.png" alt="menu" className="w-6 h-6" />
+        </NavLink>
+      </div>
     </header>
   );
 };
