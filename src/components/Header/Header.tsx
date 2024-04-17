6ac1b1d456b1f02c5b98bf3e9574fabbd57313ea
Tetiana Lisne <tplovic@gmail.com>
2024-04-17 10:35:29 +0300

    feat: fix links in nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Navigation } from './Navigation';
 
 export const Header = () => {
   return (
-    <header className="max-xl grid grid-cols-12">
+    <header className="grid grid-cols-12">
       <Navigation />
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,17 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
-      screens: {
-        sm: '320px',
-        md: '640px',
-        md: '768px',
-        lg: '1200px',
-      },
-        colors: {
-          primary: '#313237',
-          secondary: '#89939A',
-          icons: '#B4BDC3',
-          elements: '#E2E6E9',
-          'hover-bg': '#FAFBFC',
-          white: '#FFFFFF',
-          green: '#27AE60',
-          red: '#EB5757',
-        },
+      colors: {
+        primary: '#313237',
+        secondary: '#89939A',
+        icons: '#B4BDC3',
+        elements: '#E2E6E9',
+        'hover-bg': '#FAFBFC',
+        white: '#FFFFFF',
+        green: '#27AE60',
+        red: '#EB5757',
       },
     },
+  },
   plugins: [],
 };
