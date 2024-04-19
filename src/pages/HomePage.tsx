20d07b7d7a8efbec82773b1a663222f77592d036
Nikita Ganzha <geekneek28@gmail.com>
2024-04-19 19:34:37 +0300

    add promo slider

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Category } from '../components/Category/Category';
+import SliderPromo from '../components/PromoSlider/SliderPromo';
 
 export const HomePage = () => (
   <>
     <h1 className="text-5xl font-extrabold">Welcome to Nice Gadgets store!</h1>
     <section>
+      <SliderPromo />
       <Category />
     </section>
   </>
