ebc1d0a342ca8ade8bc74b9aa6e8567b8f653b17
Oleksandr Yepik <sepika534@gmail.com>
2024-04-19 19:22:38 +0300

    add logic for sort models

--- src/components/SwiperForModels/SwiperForModels.tsx
+++ src/components/SwiperForModels/SwiperForModels.tsx
@@ -8,9 +8,21 @@ import { setProducts } from '../../store/productsSlice';
 import { getProducts } from '../../api/api';
 import { Virtual } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
+import { Product } from '../../types/Product';
 
-export const SwiperComponent: React.FC = () => {
-  const { products, isLoaded } = useSelector((state: RootState) => state.products);
+interface SwiperComponentProps {
+  filterFunction: (products: Product[]) => Product[];
+  sectionTitle: string;
+}
+
+export const SwiperComponent: React.FC<SwiperComponentProps> = ({
+  filterFunction,
+  sectionTitle,
+}) => {
+  const { products, isLoaded } = useSelector((state: RootState) => ({
+    products: filterFunction(state.products.products),
+    isLoaded: state.products.isLoaded,
+  }));
   const dispatch = useDispatch();
   const sliderRef = useRef<SwiperRef>(null);
 
@@ -30,7 +42,7 @@ export const SwiperComponent: React.FC = () => {
   return (
     <div className="relative">
       <div className="flex justify-between mb-6">
-        <h1 className="text-4xl font-extrabold">Brand new models</h1>
+        <h1 className="text-4xl font-extrabold">{sectionTitle}</h1>
         <div className="flex space-x-4">
           <button className="border border-grey w-8 h-8" onClick={handlePrevious}>
             <div className="inline-flex  w-3 h-3 border-solid border-grey border-l-[3px] border-t-[3px] -rotate-45"></div>
@@ -51,16 +63,11 @@ export const SwiperComponent: React.FC = () => {
         className=" absolute "
         ref={sliderRef}
       >
-        {products
-          .filter(
-            (product) =>
-              product.category === 'phones' && product.name.includes('Apple iPhone 14' && '128'),
-          )
-          .map((product, index) => (
-            <SwiperSlide key={product.id} virtualIndex={index}>
-              <CardItem product={product} />
-            </SwiperSlide>
-          ))}
+        {products.map((product, index) => (
+          <SwiperSlide key={product.id} virtualIndex={index}>
+            <CardItem product={product} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
