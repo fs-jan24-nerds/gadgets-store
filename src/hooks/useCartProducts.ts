313cf5468fed5c513892e3905280d3d4a517af39
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-04-22 10:02:29 +0300

    add counter for icons

--- src/hooks/useCartProducts.ts
+++ src/hooks/useCartProducts.ts
@@ -1,32 +1,32 @@
-import { Product } from "../types/Product";
-import { useSelector } from "react-redux";
-import { addToCart, removeAllFromCart, removeOneFromCart } from "../store/cartSlice";
-import { RootState } from "../store/store";
-import { useDispatch } from "react-redux";
-import { CartItemType } from "../types/cart";
+import { Product } from '../types/Product';
+import { useSelector } from 'react-redux';
+import { addToCart, removeAllFromCart, removeOneFromCart } from '../store/cartSlice';
+import { RootState } from '../store/store';
+import { useDispatch } from 'react-redux';
+import { CartItemType } from '../types/cart';
 
 type HookType = {
   cart: CartItemType[];
   addProductToCart: (...productsToAdd: Product[]) => void;
-  removeOneFromCartById: (...productIdsToRemove: Product["id"][]) => void;
-  removeAllFromCartById: (...productIdsToRemove: Product["id"][]) => void;
-}
+  removeOneFromCartById: (...productIdsToRemove: Product['id'][]) => void;
+  removeAllFromCartById: (...productIdsToRemove: Product['id'][]) => void;
+};
 
 export const useCartProducts = (): HookType => {
   const dispatch = useDispatch();
-  const { cart }= useSelector((state: RootState) => state.cart);
+  const { cart } = useSelector((state: RootState) => state.cart);
 
   const addProductToCart = (productToAdd: Product) => {
     dispatch(addToCart(productToAdd));
-  }
+  };
 
   const removeOneFromCartById = (productIdToRemove: number) => {
     dispatch(removeOneFromCart(productIdToRemove));
-  }
+  };
 
   const removeAllFromCartById = (productIdToRemove: number) => {
-    dispatch(removeAllFromCart(productIdToRemove))
-  }
+    dispatch(removeAllFromCart(productIdToRemove));
+  };
 
   return {
     cart,
@@ -34,4 +34,4 @@ export const useCartProducts = (): HookType => {
     removeOneFromCartById,
     removeAllFromCartById,
   };
-}
+};
