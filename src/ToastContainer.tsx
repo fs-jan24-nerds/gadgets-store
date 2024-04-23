202a567a1ed86cac6e7f71caf41495ce219f3df9
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-04-23 13:22:41 +0300

    add smth

--- src/ToastContainer.tsx
+++ src/ToastContainer.tsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const ToastContainerWrapper: React.FC = () => {
   return (
     <ToastContainer
-      position="top-right"
+      position="bottom-right"
       autoClose={3000}
       hideProgressBar={false}
       newestOnTop={false}
