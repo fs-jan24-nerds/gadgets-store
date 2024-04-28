610bf41ee60eb2ac9d0a381cf38594ea04d53650
Nikita Donskoy <donskoy.nikita13@gmail.com>
2024-04-28 18:48:45 +0300

    fixed product details grid implementation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { Description } from '../types/Product';
 import { motion } from 'framer-motion';
 import { generateAnimation } from '../utils/animations';
+import { GridItem } from './Grid/GridItem';
 
 interface Props {
   item: {
@@ -16,68 +17,72 @@ interface Props {
 }
 
 export const About: React.FC<Props> = ({ item }) => {
+  const {
+    screen,
+    resolution,
+    processor,
+    ram,
+    camera,
+    zoom,
+    cell
+  } = item;
+
   return (
-    <div className="grid grid-cols-12 w-full mt-14 md:mt-16 lg:mt-20">
+    <>
+    <GridItem className="mb-4 laptop:mb-0 col-start-1 col-end-5 tablet:col-end-13 laptop:col-end-7 desktop:col-end-13">
       <motion.div
         initial="hidden"
         transition={{ delay: 0.7, duration: 0.6 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
         variants={generateAnimation('x', -60)}
-        className="col-span-12 pdt:col-span-6 mb-10"
+        className="w-full"
       >
-        <h1 className="font-bold text-2xl md:text-3xl text-primary lg:text-xl border-b-2 border-elements">
+        <h1 className="font-bold text-2xl md:text-3xl text-primary border-b-2 border-elements">
           About
         </h1>
         {item.description.map((desc: Description, index: number) => (
           <div key={index}>
-            <h2 className="font-bold text-xl md:text-2xl lg:text-xl mt-6 text-primary">
+            <h2 className="font-bold text-xl md:text-2xl mt-6 text-primary">
               {desc.title}
             </h2>
             <p className="mt-3 font-medium text-secondary">{desc.text}</p>
           </div>
         ))}
       </motion.div>
+      </GridItem>
+
+      <GridItem className="col-start-1 col-end-5 tablet:col-end-13 laptop:col-start-9 laptop:col-end-13 desktop:col-start-17 desktop:col-end-25">
+      
       <motion.div
         initial="hidden"
         transition={{ delay: 0.7, duration: 0.6 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
         variants={generateAnimation('x', 60)}
-        className="col-span-12 pdt:col-start-8 pdt:col-span-5 mb-10"
+        className="w-full"
       >
-        <h1 className="font-bold text-2xl md:text-3xl lg:text-xl border-b-2 border-elements text-primary">
+        <h1 className="font-bold text-2xl md:text-3xl border-b-2 border-elements text-primary">
           Tech specs
         </h1>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Screen</p>
-          <p className="text-primary">{item.screen}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary text-primary">Resolution</p>
-          <p className="text-primary">{item.resolution}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Processor</p>
-          <p className="text-primary">{item.processor}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Ram</p>
-          <p className="text-primary">{item.ram}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Camera</p>
-          <p className="text-primary">{item.camera}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Zoom</p>
-          <p className="text-primary">{item.zoom}</p>
-        </div>
-        <div className="flex justify-between mt-3 ">
-          <p className="font-medium text-secondary">Cell</p>
-          <p className="text-primary">{item.cell}</p>
-        </div>
+        {
+          Object.entries({
+            Screen: screen,
+            Resolution: resolution,
+            Processor: processor,
+            Ram: ram,
+            Camera: camera,
+            Zoom: zoom,
+            Cell: cell.join(', '),
+          }).map(([key, value]) => (
+            <div key={key} className="flex justify-between mt-3 ">
+              <p className="font-medium text-secondary">{key}</p>
+              <p className="text-primary h-2">{value}</p>
+            </div>
+          ))
+        }
       </motion.div>
-    </div>
+      </GridItem>
+    </>
   );
 };
